fdd587707d1e4949b3f8b1fec7a10ef26580db50
Christina von Flach <christina.flach@gmail.com>
2022-06-05 19:14:00 -0300

    sync

---
+++ tests/oracle/string.s
@@ -0,0 +1,16 @@
+(1,NUM,"1")
+(2,ID,"hello")
+(3,STRING,""hello"")
+(4,STRING,""hello\n"")
+(5,NUM,"2")
+(7,STRING,""hello \
+folks"")
+(8,NUM,"3")
+(9,ERROR,""")
+(9,ID,"hello")
+(10,ID,"folks")
+(10,ERROR,""")
+(11,NUM,"4")
+(14,STRING,""hello \  
+my \   
+folks!"")
